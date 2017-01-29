8dcac70bb72a6c65e942c78d4e14c080a4043547
Arvind0207 <arvindsargam@gmail.com>
2017-01-29 19:28:41 +0530

    Bot

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bot_Application3
+{
+    public class Class1
+    {
+
+
+        public String V_RetutrnString()
+        {
+             
+            return "Sargam";
+
+        }
+    }
+}
---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace Bot_Application3
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            // one of these will have an interface and process it
+
+
+            // check if activity is of type message
+            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new SandwichOrder());
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+        }
+
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+
+    }
+    public enum SandwichOptions
+    {
+        BLT, BlackForestHam, BuffaloChicken, ChickenAndBaconRanchMelt, ColdCutCombo, MeatballMarinara,
+        OvenRoastedChicken, RoastBeef, RotisserieStyleChicken, SpicyItalian, SteakAndCheese, SweetOnionTeriyaki, Tuna,
+        TurkeyBreast, Veggie
+    };
+    public enum LengthOptions { SixInch, FootLong };
+    public enum BreadOptions { NineGrainWheat, NineGrainHoneyOat, Italian, ItalianHerbsAndCheese, Flatbread };
+    public enum CheeseOptions { American, MontereyCheddar, Pepperjack };
+    public enum ToppingOptions
+    {
+        Avocado, BananaPeppers, Cucumbers, GreenBellPeppers, Jalapenos,
+        Lettuce, Olives, Pickles, RedOnion, Spinach, Tomatoes
+    };
+    public enum SauceOptions
+    {
+        ChipotleSouthwest, HoneyMustard, LightMayonnaise, RegularMayonnaise,
+        Mustard, Oil, Pepper, Ranch, SweetOnion, Vinegar
+    };
+    [Serializable]
+    public class SandwichOrder : IDialog<object>
+    {
+        protected int count = 1;
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            if (message.Text == "reset")
+            {
+                PromptDialog.Confirm(
+                    context,
+                    AfterResetAsync,
+                    "Are you sure you want to reset the count?",
+                    "Didn't get that!",
+                    promptStyle: PromptStyle.None);
+            }
+            else
+            {
+                await context.PostAsync($"{this.count++}: You said {message.Text}");
+                context.Wait(MessageReceivedAsync);
+            }
+        }
+        public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                this.count = 1;
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
